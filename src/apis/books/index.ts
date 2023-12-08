10a46961124b5314c9172ead3d7fb433a8b24db3
Emre Tufan <emretufan42@gmail.com>
2023-12-08 03:34:51 +0300

    chore: change max results on books fetch

--- src/apis/books/index.ts
+++ src/apis/books/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_API_URL } from "../../config";
 
 export const fetchBooks = async (searchQuery: string) => {
-  const { data } = await axios.get(`${BASE_API_URL}?q=${searchQuery}`);
+  const { data } = await axios.get(`${BASE_API_URL}?q=${searchQuery}&maxResults=24`);
 
   return data;
 };
